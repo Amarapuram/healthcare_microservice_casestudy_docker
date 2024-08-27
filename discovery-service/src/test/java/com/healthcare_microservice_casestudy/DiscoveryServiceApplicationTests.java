96f4ed54842f8a81ff0ab70fac8ed3345f1e0058
Amarapuram <2002arohit@gmail.com>
2024-08-27 15:08:13 +0530

    first commit

---
+++ discovery-service/src/test/java/com/healthcare_microservice_casestudy/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare_microservice_casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
